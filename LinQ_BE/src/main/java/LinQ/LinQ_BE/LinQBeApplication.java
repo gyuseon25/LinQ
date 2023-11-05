b7ade16fbfc5fa055ecc88fd16e901b7a4a7da32
gyuseon25 <mn5592@naver.com>
2023-11-06 02:30:31 +0900

    영상 2까지 완료

---
+++ LinQ_BE/src/main/java/LinQ/LinQ_BE/LinQBeApplication.java
@@ -0,0 +1,13 @@
+package LinQ.LinQ_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinQBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinQBeApplication.class, args);
+	}
+
+}
---
+++ LinQ_BE/src/test/java/LinQ/LinQ_BE/LinQBeApplicationTests.java
@@ -0,0 +1,13 @@
+package LinQ.LinQ_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinQBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
